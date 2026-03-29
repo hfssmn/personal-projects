37c0af4ddcb2e9d63f44b69899b5c1cf1ebda5f7
68646 <68646@etu.he2b.be>
2026-03-30 00:04:15 +0200

    color and animated atrs background

--- mood-tracker/script.js
+++ mood-tracker/script.js
@@ -3,16 +3,45 @@
 function setMood(mood){
     let message="";
     if (mood === "happy") {
-    message = "🌸 Courage, tu fais du bon boulot. Sois fière de toi 💖";
+    message = "Courage, tu fais du bon boulot. Sois fière de toi 💙";
   } 
   else if (mood === "tired") {
-    message = "😴 Repose-toi, c’est important. Tu fais déjà de ton mieux 💭";
+    message = "Repose-toi, c’est important. Tu fais déjà de ton mieux 🪐";
   } 
   else if (mood === "angry") {
-    message = "✨ Les épreuves de la vie passent, reste forte. Tout finit par s’apaiser 🌙";
+    message = "Les épreuves de la vie passent, reste forte. Tout finit par s’apaiser 🌙";
   }
 
   document.getElementById("result").innerText = message;
 }
+
+function createStar() {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  const symbols = ["✨", "✦", "✧"];
+
+  star.innerText = symbols[Math.floor(Math.random() * symbols.length)];
+
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 100 + "vh";
+  star.style.fontSize = Math.random() * 10 + 10 + "px";
+  star.style.animationDuration = Math.random() * 10 + 8 + "s";
+
+  document.getElementById("stars").appendChild(star);
+  
+setTimeout(() => {
+  star.remove();
+}, Math.random() * 15000 + 5000);
+  
+}
+
+// créer plein d'étoiles au début
+for (let i = 0; i < 70; i++) {
+  createStar();
+}
+
+// en rajouter en continusetInterval(createStar, 1200;
+setInterval(createStar, 1200);
 
    
